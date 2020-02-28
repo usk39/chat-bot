74ffe322fc5f8494dca1ea585b9ddfb44bbc5752
usk39 <60925409+usk39@users.noreply.github.com>
2020-02-28 22:05:02 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,68 @@
+from flask import Flask, request, abort
+
+from linebot import (
+   LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+   InvalidSignatureError
+)
+from linebot.models import (
+   MessageEvent, TextMessage, TextSendMessage,
+)
+
+import os
+import requests
+
+app = Flask(__name__)
+
+TALKAPI_KEY = os.environ['YOUR_API']
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
+def talkapi(text):
+   url = 'https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk'
+   req = requests.post(url, {'apikey':TALKAPI_KEY,'query':text}, timeout=5)
+   data = req.json()
+
+   if data['status'] != 0:
+      return data['message']
+
+   msg = data['results'][0]['reply']
+   return msg
+
+@app.route("/")
+def hello_world():
+   return "hello world!"
+
+@app.route("/callback", methods=['POST'])
+def callback():
+   # get X-Line-Signature header value
+   signature = request.headers['X-Line-Signature']
+
+   # get request body as text
+   body = request.get_data(as_text=True)
+   app.logger.info("Request body: " + body)
+
+   # handle webhook body
+   try:
+       handler.handle(body, signature)
+   except InvalidSignatureError:
+       print("Invalid signature. Please check your channel access token/channel secret.")
+       abort(400)
+
+   return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+   push_text = event.message.text
+   msg = talkapi(push_text)
+   line_bot_api.reply_message(
+       event.reply_token,
+       TextSendMessage(text=msg))
+
+if __name__ == "__main__":
+   port = int(os.getenv("PORT"))
+   app.run(host="0.0.0.0", port=port)
