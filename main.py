75144135766e64572d47dd966d026e34a50ce960
usk39 <60925409+usk39@users.noreply.github.com>
2020-02-28 22:09:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ import requests
 
 app = Flask(__name__)
 
-TALKAPI_KEY = os.environ['YOUR_API']
-YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
-YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+TALKAPI_KEY = os.environ['TALKAPI_KEY']
+LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
+LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
 
-line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
 def talkapi(text):
    url = 'https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk'
